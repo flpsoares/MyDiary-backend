dd982a08e0e82628e612fd445c6dc0e3bc4f1f19
Filipe Soares <filipeseventeen1@gmail.com>
2021-04-30 14:25:51 -0300

    Adicionado o validator no controller de create user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,23 +1,36 @@
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 
 export default class UsersController {
-  public async index({  }: HttpContextContract) {
+  public async index(){
     const users =  User.all()
 
     return users
   }
 
   public async store({ request }: HttpContextContract) {
-    const {username, password, email} = request.all()
-
-    const user = await User.create({
-      username,
-      password,
-      email
+    const validatedData = await request.validate({
+      schema: schema.create({
+        username: schema.string({}, [
+          rules.unique({table: 'users', column: 'username'})
+        ]),
+        password: schema.string(),
+        email: schema.string({}, [
+          rules.unique({table: 'users', column: 'email'}),
+          rules.email()
+        ]),
+      }),
+      messages: {
+        required: 'Make sure to enter the all field value',
+        'username.unique': 'Username is already in use',
+        'email.unique': 'Email is already in use',
+        'email.email': 'Invalid email'
+      }
     })
+
+    const user = User.create(validatedData)
 
     return user
   }
